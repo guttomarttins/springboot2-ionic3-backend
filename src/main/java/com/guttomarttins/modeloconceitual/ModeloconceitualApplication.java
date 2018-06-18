2d4ca05138868c160d4392271144d4d185640082
Carlos Martins <guttomarttins@gmail.com>
2018-06-18 15:44:24 -0300

    Projeto criado

---
+++ src/main/java/com/guttomarttins/modeloconceitual/ModeloconceitualApplication.java
@@ -0,0 +1,12 @@
+package com.guttomarttins.modeloconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloconceitualApplication.class, args);
+	}
+}
