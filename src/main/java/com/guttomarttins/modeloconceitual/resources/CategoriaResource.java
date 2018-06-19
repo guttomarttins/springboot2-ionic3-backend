d89584b1d03b7fe3e987cb1e78a7862127087679
Carlos Martins <guttomarttins@gmail.com>
2018-06-19 14:57:11 -0300

    Criando repositorio para categoria

--- src/main/java/com/guttomarttins/modeloconceitual/resources/CategoriaResource.java
+++ src/main/java/com/guttomarttins/modeloconceitual/resources/CategoriaResource.java
@@ -1,29 +1,40 @@
 package com.guttomarttins.modeloconceitual.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guttomarttins.modeloconceitual.domain.Categoria;
+import com.guttomarttins.modeloconceitual.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	private List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	private ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
+	
+//	@RequestMapping(method=RequestMethod.GET)
+//	private List<Categoria> listar() {
+//		
+//		Categoria cat1 = new Categoria(1, "Informática");
+//		Categoria cat2 = new Categoria(2, "Escritório");
+//		
+//		List<Categoria> lista = new ArrayList<>();
+//		lista.add(cat1);
+//		lista.add(cat2);
+//		
+//		return lista;
+//	}
 	
 }
---
+++ src/main/java/com/guttomarttins/modeloconceitual/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.guttomarttins.modeloconceitual.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guttomarttins.modeloconceitual.domain.Categoria;
+import com.guttomarttins.modeloconceitual.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
