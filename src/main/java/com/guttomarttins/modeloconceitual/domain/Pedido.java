656b9bc627b715f4cf2bcf1e9ef4639c4ee1566d
Carlos Martins <guttomarttins@gmail.com>
2018-06-25 20:51:25 -0300

    Atualizacao annotation json usando somente jsonignore

--- src/main/java/com/guttomarttins/modeloconceitual/domain/Pedido.java
+++ src/main/java/com/guttomarttins/modeloconceitual/domain/Pedido.java
@@ -15,8 +15,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Pedido implements Serializable {
 
@@ -28,11 +26,9 @@ public class Pedido implements Serializable {
 	
 	private Date instante;
 
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
