be30b15bfc25d9f0c763a943ca2837b022286972
Carlos Martins <guttomarttins@gmail.com>
2018-06-25 20:41:29 -0300

    Add endpoint pedidos disponibilizado

---
+++ src/main/java/com/guttomarttins/modeloconceitual/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package com.guttomarttins.modeloconceitual.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guttomarttins.modeloconceitual.domain.Pedido;
+import com.guttomarttins.modeloconceitual.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	private ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	 
+	
+}
