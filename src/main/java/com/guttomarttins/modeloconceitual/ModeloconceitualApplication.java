d1c7e97adc49f76a472952b8761d2b8dc1b3b643
Carlos Martins <guttomarttins@gmail.com>
2018-06-25 09:36:11 -0300

    Add ItemPedido e ItemPedidoPK

--- src/main/java/com/guttomarttins/modeloconceitual/ModeloconceitualApplication.java
+++ src/main/java/com/guttomarttins/modeloconceitual/ModeloconceitualApplication.java
@@ -13,6 +13,7 @@ import com.guttomarttins.modeloconceitual.domain.Cidade;
 import com.guttomarttins.modeloconceitual.domain.Cliente;
 import com.guttomarttins.modeloconceitual.domain.Endereco;
 import com.guttomarttins.modeloconceitual.domain.Estado;
+import com.guttomarttins.modeloconceitual.domain.ItemPedido;
 import com.guttomarttins.modeloconceitual.domain.Pagamento;
 import com.guttomarttins.modeloconceitual.domain.PagamentoComBoleto;
 import com.guttomarttins.modeloconceitual.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.guttomarttins.modeloconceitual.repositories.CidadeRepository;
 import com.guttomarttins.modeloconceitual.repositories.ClienteRepository;
 import com.guttomarttins.modeloconceitual.repositories.EnderecoRepository;
 import com.guttomarttins.modeloconceitual.repositories.EstadoRepository;
+import com.guttomarttins.modeloconceitual.repositories.ItemPedidoRepository;
 import com.guttomarttins.modeloconceitual.repositories.PagamentoRepository;
 import com.guttomarttins.modeloconceitual.repositories.PedidoRepository;
 import com.guttomarttins.modeloconceitual.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class ModeloconceitualApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloconceitualApplication.class, args);
@@ -112,6 +116,19 @@ public class ModeloconceitualApplication implements CommandLineRunner {
         
         pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
         pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+        
+        ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+        ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+        ped2.getItens().addAll(Arrays.asList(ip3));
+        
+        p1.getItens().addAll(Arrays.asList(ip1));
+        p2.getItens().addAll(Arrays.asList(ip3));
+        p3.getItens().addAll(Arrays.asList(ip2));
+        
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 }
--- src/main/java/com/guttomarttins/modeloconceitual/domain/Produto.java
+++ src/main/java/com/guttomarttins/modeloconceitual/domain/Produto.java
@@ -2,7 +2,9 @@ package com.guttomarttins.modeloconceitual.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -24,22 +27,31 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name="CATEGORIA_PRODUTO",
-	joinColumns=@JoinColumn(name="produto_id"),
-	inverseJoinColumns = @JoinColumn(name="categoria_id")
-	)
+	@JoinTable(name = "CATEGORIA_PRODUTO", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
-	public Produto() {}
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	public Produto() {
+	}
 
 	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>(); 
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -69,9 +81,17 @@ public class Produto implements Serializable {
 	public List<Categoria> getCategorias() {
 		return categorias;
 	}
-	
+
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
@@ -98,8 +118,5 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
-	 
-
 
 }
