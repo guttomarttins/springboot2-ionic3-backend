8dc24259a5edaa6a538d441e5c78ecf1e1b86895
Carlos Martins <guttomarttins@gmail.com>
2018-06-26 07:09:04 -0300

    Inserindo categoria vai post

--- src/main/java/com/guttomarttins/modeloconceitual/resources/CategoriaResource.java
+++ src/main/java/com/guttomarttins/modeloconceitual/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.guttomarttins.modeloconceitual.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guttomarttins.modeloconceitual.domain.Categoria;
 import com.guttomarttins.modeloconceitual.services.CategoriaService;
@@ -22,6 +26,13 @@ public class CategoriaResource {
 	private ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	private ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 //	@RequestMapping(method=RequestMethod.GET)
