78913282a829ff9a7b66d4c8625dbc1f1c7e1f74
Carlos Martins <guttomarttins@gmail.com>
2018-06-27 00:41:43 -0300

    Inserindo cliente via post

--- src/main/java/com/guttomarttins/modeloconceitual/resources/ClienteResource.java
+++ src/main/java/com/guttomarttins/modeloconceitual/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.guttomarttins.modeloconceitual.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guttomarttins.modeloconceitual.domain.Cliente;
 import com.guttomarttins.modeloconceitual.dto.ClienteDTO;
+import com.guttomarttins.modeloconceitual.dto.ClienteNewDTO;
 import com.guttomarttins.modeloconceitual.services.ClienteService;
 
 @RestController
@@ -30,6 +33,14 @@ public class ClienteResource {
 	private ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	private ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
