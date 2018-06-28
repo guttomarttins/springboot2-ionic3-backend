395cc93188484d49dfd4dcddf460583a1c96c533
Carlos Martins <guttomarttins@gmail.com>
2018-06-27 21:00:41 -0300

    Validacao customizada de email nao repedido na atualizacao de cliente

--- src/main/java/com/guttomarttins/modeloconceitual/dto/ClienteDTO.java
+++ src/main/java/com/guttomarttins/modeloconceitual/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.guttomarttins.modeloconceitual.domain.Cliente;
+import com.guttomarttins.modeloconceitual.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
