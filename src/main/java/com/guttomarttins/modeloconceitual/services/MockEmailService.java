d63b986dcbe0447af9a119d8d25a1c3b00c26aae
Carlos Martins <guttomarttins@gmail.com>
2018-06-30 11:12:00 -0300

    Add Email HTML

--- src/main/java/com/guttomarttins/modeloconceitual/services/MockEmailService.java
+++ src/main/java/com/guttomarttins/modeloconceitual/services/MockEmailService.java
@@ -1,13 +1,15 @@
 package com.guttomarttins.modeloconceitual.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
-public class MockEmailService extends AbstractEmailService{
+public class MockEmailService extends AbstractEmailService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
-	
+
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
@@ -15,6 +17,11 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info("Email enviado");
 	}
 
- 
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+	}
 
 }
