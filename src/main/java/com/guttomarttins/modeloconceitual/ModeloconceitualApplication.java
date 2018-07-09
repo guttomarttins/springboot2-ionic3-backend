9eda1a217435fd6bad0edd9a717e02dd16b36840
Carlos Martins <guttomarttins@gmail.com>
2018-07-09 12:48:12 -0300

    Salvando arquivo no S3

--- src/main/java/com/guttomarttins/modeloconceitual/ModeloconceitualApplication.java
+++ src/main/java/com/guttomarttins/modeloconceitual/ModeloconceitualApplication.java
@@ -1,11 +1,17 @@
 package com.guttomarttins.modeloconceitual;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guttomarttins.modeloconceitual.services.S3Service;
+
 @SpringBootApplication
 public class ModeloconceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloconceitualApplication.class, args);
@@ -13,6 +19,6 @@ public class ModeloconceitualApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uoloadFile("C:\\meustestes\\logo.png");
 	}
 }
