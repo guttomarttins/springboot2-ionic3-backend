e28405ca7e6c4ea39a9e7889acccda671df67998
Carlos Martins <guttomarttins@gmail.com>
2018-07-10 18:03:13 -0300

    Endpoint para buscar estados e cidades por estado

--- src/main/java/com/guttomarttins/modeloconceitual/repositories/CidadeRepository.java
+++ src/main/java/com/guttomarttins/modeloconceitual/repositories/CidadeRepository.java
@@ -1,11 +1,18 @@
 package com.guttomarttins.modeloconceitual.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.guttomarttins.modeloconceitual.domain.Cidade;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+	@Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
+	List<Cidade> findCidades(@Param("estadoId") Integer estadoId);
 
 }
